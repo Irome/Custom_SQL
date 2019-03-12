5edbe867b4733bfa7bb8d440318e864c2217a1bd
Micrah <mic_24@live.com>
2019-03-12 21:53:28 +1100

    1 line will be much cleaner i think

--- 2_disableFlying_BG_Arena.sql
+++ 2_disableFlying_BG_Arena.sql
@@ -1,8 +1,5 @@
 DELETE FROM `disables` WHERE `sourceType`=0 AND `entry`=31700;
-DELETE FROM `disables` WHERE `sourceType`=3 AND `entry`=31700;
 
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 31700, 17, '489,529,30,566,607,628', '', 'disable BattleGrounds Azeroth Flying');
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 31700, 49, '618,617', '3702,85,3518,3522,3698', 'disable arena Azeroth Flying');
-
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 31700, 49, '489,529,30,566,607,628,618,617', '3702,85,3518,3522,3698', 'disable Azeroth Flying in Arena and battlegrounds');
 
 
