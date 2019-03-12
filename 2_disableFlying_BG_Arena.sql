ce908da95880b9602e1403a116817ccc0e1ca4f9
Micrah <mic_24@live.com>
2019-03-12 21:34:25 +1100

    update

--- 2_disableFlying_BG_Arena.sql
+++ 2_disableFlying_BG_Arena.sql
@@ -1,4 +1,8 @@
 DELETE FROM `disables` WHERE `sourceType`=0 AND `entry`=31700;
 DELETE FROM `disables` WHERE `sourceType`=3 AND `entry`=31700;
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 31700, 1, '', '', 'Azeroth Flying');
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 31700, 17, '489,529,30,566,607,628', '', 'Azeroth Flying');
+
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 31700, 17, '489,529,30,566,607,628', '', 'disable BattleGrounds Azeroth Flying');
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 31700, 49, '618,617', '3702,85,3518,3522,3698', 'disable arena Azeroth Flying');
+
+
+
