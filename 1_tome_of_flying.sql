af65e2dbad70e43a444c90f61ea98314e942ef29
Micrah <44911744+milestorme@users.noreply.github.com>
2019-03-12 22:12:38 +1100

    update

--- 1_tome_of_flying.sql
+++ 1_tome_of_flying.sql
@@ -55,11 +55,12 @@ INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `Exte
 ('601047','0','700999','0','0','0');
 
 -- --------------------------------------------------------------------------------------
---	DISABLE BG FLYING
+--	DISABLE BG / ARENA FLYING
 -- --------------------------------------------------------------------------------------
 DELETE FROM `disables` WHERE `sourceType`=0 AND `entry`=31700;
-DELETE FROM `disables` WHERE `sourceType`=3 AND `entry`=31700;
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (3, 31700, 1, '', '', 'disable battleground Azeroth Flying');
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 31700, 49, '618,617', '3702,85,3518,3522', 'disable arena Azeroth Flying');
+
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 31700, 49, '489,529,30,566,607,628,618,617', '3702,85,3518,3522,3698', 'disable Azeroth Flying in Arena and battlegrounds');
+
+
 
 -- END OF LINE
